93770bbc074ba056825dcf1f7c4de1ffbb944c88
theMkeyholder <51534449+theMkeyholder@users.noreply.github.com>
2020-07-28 22:15:14 -0400

    this took a while

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ setInterval(update, 100);
 function matrixUp() {
   bms.push(0);
 }
+function checkVals(a,b) {
+  if(bms.slice(bms.length-(factor*a.length), bms.length).join(",")==(a.join(",")+",").repeat(factor-1)+a.join(",")) {
+    while(bms.slice(bms.length-(a.length), bms.length).join(",")==(a).join(",")) {
+      for(let i=0;i<a.length;i++) bms.pop()
+    }; 
+    bms=bms.concat(b)
+  }
+}
 function maxMatrix() {
-  if(bms.slice(bms.length-factor, bms.length).join(",")=="0,".repeat(factor-1)+"0") {while(bms[bms.length-1] == 0) bms.pop(); bms.push(0); bms.push(1)}
+  checkVals([0],[0,1])
+  checkVals([0,1],[0,1,1])
+  checkVals([0,1,1],[0,1,1,1])
+  checkVals([0,1,1,1],[0,1,1,1,1])
+  checkVals([0,1,1,1,1],[0,1,1,1,1,1])
+  checkVals([0,1,1,1,1,1],[0,1,1,1,1,1,1])
+  checkVals([0,1,1,1,1,1,1],[0,1,1,1,1,1,1,1])
+  checkVals([0,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1,1])
+  checkVals([0,1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1,1,1])
+  checkVals([0,1,1,1,1,1,1,1,1,1],[0,1,2])
 }
 function displayMatrix() {
   display = ``;
